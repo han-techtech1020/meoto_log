a428226df9728ca6ff2b105ddc8a7c7cc0b92f7e
han-techtech1020 <kishohatano@gmail.com>
2025-11-25 18:33:07 +0900

    rubocop

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,15 +1,15 @@
 class HomesController < ApplicationController
   def index
-    if user_signed_in?
-      # 1. 妻のHP（既存）
-      @latest_status = current_user.partner_statuses.last
-      @partner_status = PartnerStatus.new
+    return unless user_signed_in?
 
-      # 2. 直近の振り返り（最新の相談1件を取得）
-      @latest_consultation = current_user.consultations.last
+    # 1. 妻のHP（既存）
+    @latest_status = current_user.partner_statuses.last
+    @partner_status = PartnerStatus.new
 
-      # 3. 本日の予定（今日の0:00〜23:59の予定を取得）
-      @today_schedules = current_user.schedules.where(start_time: Time.zone.now.all_day).order(:start_time)
-    end
+    # 2. 直近の振り返り（最新の相談1件を取得）
+    @latest_consultation = current_user.consultations.last
+
+    # 3. 本日の予定（今日の0:00〜23:59の予定を取得）
+    @today_schedules = current_user.schedules.where(start_time: Time.zone.now.all_day).order(:start_time)
   end
 end
