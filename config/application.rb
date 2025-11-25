b8a0c5093b1e908c9668756ef17d2b63f42d67a3
han-techtech1020 <kishohatano@gmail.com>
2025-11-25 18:03:56 +0900

    スケジュールモデル

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module MeotoLog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # ▼ これを追加！（日本時間にする設定）
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_24_092051) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_25_081529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_24_092051) do
     t.index ["user_id"], name: "index_partner_statuses_on_user_id"
   end
 
+  create_table "schedules", force: :cascade do |t|
+    t.string "title"
+    t.datetime "start_time"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_schedules_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -48,4 +57,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_24_092051) do
 
   add_foreign_key "consultations", "users"
   add_foreign_key "partner_statuses", "users"
+  add_foreign_key "schedules", "users"
 end
