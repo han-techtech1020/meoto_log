174a95a601345ade0d88fccbafd43ab430bbce9c
han-techtech1020 <kishohatano@gmail.com>
2025-11-28 11:30:43 +0900

    修正

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,10 +1,10 @@
 class Mood < ActiveHash::Base
   self.data = [
-    { id: 1, name: '最高（女神）' },
-    { id: 2, name: '普通（平穏）' },
-    { id: 3, name: 'やや不機嫌（要注意）' },
-    { id: 4, name: '不機嫌（鬼のような人）' },
-    { id: 5, name: '激怒（人のような鬼）' }
+    { id: 1, name: '仏様（全て許される）' },
+    { id: 2, name: '神様（にこやかである）' },
+    { id: 3, name: '平穏（穏やかである）' },
+    { id: 4, name: '修羅（近づきがたい）' },
+    { id: 5, name: '厄災（離れた方がよい）' }
   ]
 
   include ActiveHash::Associations
