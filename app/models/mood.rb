539c631f81293d2df4cb83d91fc2ac5a9bf7f74d
han-techtech1020 <kishohatano@gmail.com>
2025-12-01 11:58:50 +0900

    view修正

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,7 +1,7 @@
 class Mood < ActiveHash::Base
   self.data = [
-    { id: 1, name: '仏様（全て許される）' },
-    { id: 2, name: '神様（にこやかである）' },
+    { id: 1, name: '神様（全て許される）' },
+    { id: 2, name: '天使（にこやかである）' },
     { id: 3, name: '平穏（穏やかである）' },
     { id: 4, name: '修羅（近づきがたい）' },
     { id: 5, name: '厄災（離れた方がよい）' }
