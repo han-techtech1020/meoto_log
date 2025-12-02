88b98a962cd466937c3e751a5a034fb2afbd7ede
han-techtech1020 <kishohatano@gmail.com>
2025-12-02 11:07:21 +0900

    ゲストログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ApplicationRecord
   has_many :schedules
 
   validates :nickname, presence: true
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64 # ランダムなパスワード
+      user.nickname = 'ゲストさん' # 必須項目なので設定
+      user.partner_name = '奥さん' # デフォルトの呼び名
+      # もし性格なども入れたければここに追加できます
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # 1. コントローラーを指定する
+  devise_for :users, controllers: {
+  registrations: 'users/registrations',
+  sessions: 'users/sessions'
+  }
+
+  # 2. ゲストログイン用のURLを追加
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   root to: 'homes#index'
   resources :partner_statuses, only: [:create]
   resources :consultations, only: [:index, :create, :destroy]
