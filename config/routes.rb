b28f5c7a16405d92284e48b32ae617db404104d6
han-techtech1020 <kishohatano@gmail.com>
2025-12-03 16:23:32 +0900

    ログアウト画面編集

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
+
+  # 3. 各ルーティング
   root to: 'homes#index'
   resources :partner_statuses, only: [:create]
   resources :consultations, only: [:index, :create, :destroy]
