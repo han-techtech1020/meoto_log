23c2c71b8f1fdf4a68ab52de8a7ded9d3ef867fe
han-techtech1020 <kishohatano@gmail.com>
2025-12-05 15:22:46 +0900

    修正

--- app/models/partner_status.rb
+++ app/models/partner_status.rb
@@ -3,9 +3,8 @@ class PartnerStatus < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :mood
-  belongs_to :cycle
+  belongs_to :cycle, optional: true
 
   validates :hp_percentage, presence: true
   validates :mood_id, presence: true
-  validates :cycle_id, presence: true
 end
